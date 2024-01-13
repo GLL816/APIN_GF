266a8999eb33903588243a118d8f76dbad17577d
GLL816 <980906011@qq.com>
2024-01-13 19:25:07 +0800

    Update phase_extraction.m

--- Phase extraction/phase_extraction.m
+++ Phase extraction/phase_extraction.m
@@ -51,7 +51,7 @@ for i=1:length(filelist)
     for L=1:kk
         xx=double(yy(L,:)');  
         xxx=enframe(xx,256,128);  
-        [rp,ps]=relative_phase_gll(xxx,fs);
+        [rp,ps]=DRP(xxx,fs);
         z(L,:,:)=rp(1:129,:)';
     end
     DRP{i,:}=z;
